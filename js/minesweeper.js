0275f2da75899111cbece8f5ef4592b55f8197fe
gritteh <gritteh@gmail.com>
2017-10-21 17:08:54 +0100

    more comments

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -7,15 +7,17 @@ $(document).ready(function() {
 
     let mineArea = $(".minesweeper__area");
 
+    // When form submitted
     genForm.on("submit", (e) => {
+        // Prevent page from refreshing
         e.preventDefault;
+        // Collect input values
         let numberOfRows = rowInput.val();
         let numberOfCols = colInput.val();
         let numberOfMines = mineInput.val();
 
-
+        // Generate minesweeper table
         minesweeperGenerate(numberOfRows, numberOfCols, numberOfMines);
-
     });
 
     // Gives single cell with css positioning
@@ -30,13 +32,13 @@ $(document).ready(function() {
         return cell;
     };
 
-    // array to contain HTML elements
+    // Array to contain HTML elements (cells)
     let htmlArray = [];
 
+    // Generate minesweeper table function, takes number of rows, columns and mines as params
     const minesweeperGenerate = (rows, columns, mines) => {
+        // Calculate total number of cells
         let numberOfCells = rows * columns;
-
-        
 
         // Loop to generate the right number of cells
         for (let i = 0; i < numberOfCells; i++) {
@@ -44,13 +46,14 @@ $(document).ready(function() {
             // Add html to array for later reference
             htmlArray.push(cell);
         }
-        // Map HTML array to HTML
+        // Append HTML elements to HTML minesweeper area
         htmlArray.map((val, i) => {
             mineArea.append(val);
         });
 
         // Count for recording number of X's 
         let count = 0;
+        // Array for collecting indexes of where mines are placed
         let placementPast = [];
         // Place the right number of mines
         while (count < mines) {
@@ -60,7 +63,7 @@ $(document).ready(function() {
             if ($.inArray(randomNumber, placementPast) === -1) {
                 // Add number to array to mark that as used
                 placementPast.push(randomNumber);
-                
+                // Place mines
                 htmlArray[randomNumber].text("X").addClass("marked");
                 count++;
             } else {
@@ -69,24 +72,30 @@ $(document).ready(function() {
             }
         }
 
-        // use placements to add 1 to data("clue") value of elements around X
+        // Use mine placements to add 1 to data("clue") value of elements around mine
+
+        // Set the data("clue") value to 0 for all cells
         htmlArray.map(val => val.data("clue", 0));
 
         placementPast.map((val, i) => {
+            // Give placements (indexes) of mines to giveSurroundingCells, which gives addToData an array of indexes of surrounding cells
             addToData(giveSurroundingCells(val, columns, numberOfCells));
-            
         });
+
+        // After clues have been counted, display the numbers above 0
         displayNumbers();
 
     };
 
 
-    // Function to give indexes of surrounding cells
+    // Function to give indexes of surrounding cells (3x3 around cell)
     const giveSurroundingCells = (index, columns, totalCells) => {
         let indexArray = [];
 
+        // Row with mine
         let markedRow = Math.floor(index / columns);
 
+        // Declaring indexes of surrounding cells
         let topLeft = index - columns - 1;
         let topMiddle = index - columns;
         let topRight = index - columns + 1;
@@ -96,7 +105,7 @@ $(document).ready(function() {
         let bottomMiddle = index + +columns;
         let bottomRight = index + +columns + 1;
 
-
+        // Only add index to array if they are on the correct row in relation to the mine
         markedRow === Math.floor(topLeft / columns) + 1 ? indexArray.push(topLeft) : null;
         markedRow === Math.floor(topMiddle / columns) + 1 ? indexArray.push(topMiddle) : null;
         markedRow === Math.floor(topRight / columns) + 1 ? indexArray.push(topRight) : null;
@@ -106,6 +115,7 @@ $(document).ready(function() {
         markedRow === Math.floor(bottomMiddle / columns) - 1 ? indexArray.push(bottomMiddle) : null;
         markedRow === Math.floor(bottomRight / columns) - 1 ? indexArray.push(bottomRight) : null;
 
+        // Filter to remove negative indexes and ones that are too high (don't exist)
         let filteredArray = indexArray.filter((number) => {
             return (number >= 0 && number < totalCells);
         });
@@ -113,9 +123,10 @@ $(document).ready(function() {
         return filteredArray;
     };
 
-    // Function to add to one to data("clue") of cells surrounding mines
+    // Function to add 1 to data("clue") of cells surrounding mines - takes array of indexes of cells to add 1 to
     const addToData = (arrayOfIndexes) => {
         arrayOfIndexes.map((val, i) => {
+            // Add 1 to previous data("clue") value of cell
             htmlArray[val].data("clue", htmlArray[val].data("clue") + 1);
         });
     };
